095e457d41e8496063854278fe33eeb1129bfd9e
DanP05 <berti.penaguzman@edu.sait.ca>
2024-11-28 16:46:29 -0800

    Video thing
    page almost done just need to fix the video and some other small things

---
+++ power.js
@@ -0,0 +1,28 @@
+plan_ning.addEventListener('click',function(){
+    console.log("Hola me llamo Daniel");
+    document.getElementById("image").src="backpack.JPG"
+    document.getElementById("MyH2").textContent="Planning"
+    document.getElementById("MyP").textContent="It all began with a childhood dream of exploring the world. After months of scrolling through travel blogs, I finally booked my tickets. Without knowing, packing for a 2-month trip into a single backpack would be my first big challenge."
+    
+
+})
+
+aventuras.addEventListener('click',function(){
+    console.log("Hola me llamo Daniel");
+    document.getElementById("image").src="rome.MP4"
+    document.getElementById("MyH2").textContent="Adventures"
+    document.getElementById("MyP").textContent="During those two months of travelling through different countries, I had so many adventures, but the most remarkable one was the first time I saw the Colosseum in Rome. I had planned to take a casual walk while looking for a restaurant to enjoy some pasta. Suddenly, I found myself crossing a park, and at the end of it, I saw something enormous that immediately caught my attention, it was the Colosseum. It was such an inspiring and unforgettable moment in my journey."
+
+})
+luchas.addEventListener('click',function(){
+    console.log("Hola me llamo Daniel");
+    document.getElementById("image").src="phone.JPG"
+    document.getElementById("MyH2").textContent="Challenges"
+    document.getElementById("MyP").textContent="On my way to the airport in Amsterdam, I was following Google Maps. But as soon as the train departed, I realized I was on the wrong one. It was heading in the opposite direction. I panicked and set in as I frantically started asking people if I was on the right train. Thankfully, a kind local helped me figure it out. I got off at the next stop and managed to catch the correct train to the airport just in time."
+})
+lecciones.addEventListener('click',function(){
+    console.log("Hola me llamo Daniel");
+    document.getElementById("image").src="ocean.jpeg"
+    document.getElementById("MyH2").textContent="Takeaways"
+    document.getElementById("MyP").textContent="When my plane took off from Amsterdam, I looked out over the vast North Sea. This trip wasn’t just a vacation; it was a lesson in independence, spontaneity, and the beauty of connecting with new cultures. I returned home with a full heart and a growing list of destinations for my next adventure."
+})
